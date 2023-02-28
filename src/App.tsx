1765a806e97083e351b4f79c70f7c183a34c8ad2
Murilo Martins <murilomartins@outlook.com>
2023-02-28 17:48:03 -0300

    :books: Header component, Home and HomeIndex route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeIndex from "./routes/Home/HomeIndex";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />}>
+          <Route index element={<HomeIndex />}/>
+          <Route path="home" element={<Navigate to={"/"} />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/Home/HomeIndex/index.tsx
@@ -0,0 +1,9 @@
+export default function HomeIndex() {
+  return (
+    <main>
+      <h1>Desafio Github API</h1>
+      <h2>DevSuperior - Escola de programação</h2>
+      <div>Button</div>
+    </main>
+  );
+}
