b2afeabb01a757a67b3083cd111346a1501c0505
Murilo Martins <murilomartins@outlook.com>
2023-03-01 15:39:20 -0300

    :books: HomeIndex CSS Fix

--- src/routes/Home/HomeIndex/index.tsx
+++ src/routes/Home/HomeIndex/index.tsx
@@ -1,14 +1,19 @@
+import "./styles.css";
 import { Link } from "react-router-dom";
 import Button from "../../../components/Button";
 
 export default function HomeIndex() {
   return (
     <main>
-      <h1>Desafio Github API</h1>
-      <h2>DevSuperior - Escola de programação</h2>
-      <Link to={"/search-profile"}>
-        <Button text="Começar" />
-      </Link>
+      <section id="rrc-home">
+        <div className="rrc-home-titles">
+          <h1>Desafio Github API</h1>
+          <h2>DevSuperior - Escola de programação</h2>
+        </div>
+        <Link to={"/search-profile"}>
+          <Button text="Começar" />
+        </Link>
+      </section>
     </main>
   );
 }
